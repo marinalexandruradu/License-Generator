c7a0ff2f93bd54df6d17bde226200a36606b0c93
Alex Marin <alex.marin@caphyon.com>
2021-03-23 23:49:10 +0200

    Initial Release

---
+++ LicenseGenerator/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LicenseGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string test = Program.GenerateRandom();
+            string test2 = Program.GenerateUnix();
+
+            textBox1.Text = test +"-"+test2;
+            toolStripStatusLabel1.Text = "Key Generated";
+            statusStrip1.BackColor = Color.Green;
+            statusStrip1.ForeColor = Color.White;
+        }
+
+       private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int NrOfKeys = 0;
+            if (Int32.TryParse(textBox2.Text, out NrOfKeys))
+            {
+                toolStripStatusLabel1.Text = NrOfKeys.ToString() + " keys have been generated";
+                statusStrip1.BackColor = Color.Green;
+                statusStrip1.ForeColor = Color.White;
+
+                for (int i = 0; i <= NrOfKeys; i++)
+                {
+                    string test3 = Program.GenerateRandom();
+                    string test4 = Program.GenerateUnix();
+                    Console.WriteLine(test3 + "-" + test4);
+                    using (StreamWriter w = File.AppendText("cdkeys.txt"))
+                    {
+                        w.WriteLine(test3 + "-" + test4);
+                    }
+                }
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "Please input a number";
+                statusStrip1.BackColor = Color.Red;
+                statusStrip1.ForeColor = Color.White;
+            }
+
+            
+
+        }
+    }
+}
---
+++ LicenseGenerator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LicenseGenerator
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+        public static string AddCheckDigit(string number)
+        {
+            int Sum = 0;
+            for (int i = number.Length - 1, Multiplier = 2; i >= 0; i--)
+            {
+                Sum += (int)char.GetNumericValue(number[i]) * Multiplier;
+
+                if (++Multiplier == 8) Multiplier = 2;
+            }
+            string Validator = (11 - (Sum % 11)).ToString();
+
+            if (Validator == "11") Validator = "0";
+            else if (Validator == "10") Validator = "X";
+
+            return number + Validator;
+        }
+
+        public static string GenerateRandom() {
+            Random rnd = new Random();
+            int digitcode = rnd.Next(100000000, 999999999);
+
+            string myString = digitcode.ToString();
+            string test2 = AddCheckDigit(myString);
+            return test2;
+        }
+
+        public static string GenerateUnix() {
+            Random rnd = new Random();
+            int month = rnd.Next(1, 364);
+            int digitcode = rnd.Next(100000000, 999999999);
+            DateTime dt = new DateTime(2021, 1, 1);
+            DateTime newDate = dt.AddDays(month);
+
+            Int32 unixTimestamp = (Int32)(newDate.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            string myUnixString = unixTimestamp.ToString();
+            return myUnixString;
+        }
+
+        
+        static void Main()
+        {
+
+            //Random rnd = new Random();
+            //int month = rnd.Next(1, 364);
+            //int digitcode = rnd.Next(100000000, 999999999);
+            //DateTime dt = new DateTime(2021, 1, 1);
+            //DateTime newDate = dt.AddDays(month);
+
+            //Int32 unixTimestamp = (Int32)(newDate.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
+            //Console.WriteLine(unixTimestamp);
+            //string myString = digitcode.ToString();
+            //string myUnixString = unixTimestamp.ToString();
+
+            //System.DateTime dtDateTime = new DateTime(1970, 1, 1);
+            //dtDateTime = newDate.AddMilliseconds(unixTimestamp).ToLocalTime();
+            //int year = dtDateTime.Year;
+            //if  (year = 2021) { }
+            //Console.WriteLine(dtDateTime);
+            //Console.WriteLine(year);
+
+            //string test2 = AddCheckDigit(myString);
+
+            //Console.WriteLine(test2);
+            //Console.WriteLine(tessst);
+
+            //Console.WriteLine("Serial key:" + myUnixString + "-" + myString + "-");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
